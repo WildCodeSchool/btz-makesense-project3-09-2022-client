553bb056790680239ef1a58709737dfbdeeb845d
Tony Creavin <tonycreavin@gmail.com>
2022-12-21 14:08:09 +0100

    Forgotten password page created

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,7 @@
-import Link from "next/link";
-
 export default function Home() {
   return (
     <div className="bg-black">
       <div>TEMPLATE</div>
-      <Link href="./signin">Signin</Link>
     </div>
   );
 }
