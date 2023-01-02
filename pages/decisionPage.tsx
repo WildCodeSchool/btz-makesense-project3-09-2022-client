7b241647e00b4315cb026cda3ca6718e7793d288
Tony Creavin <tonycreavin@gmail.com>
2023-01-02 17:24:58 +0100

    decision page done

---
+++ pages/decisionPage.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Card from "./components/Card";
+
+export default function decisionPage() {
+  return (
+    <div>
+      <Navbar />
+      <div className="w-screen h-screen bg-[#24673A]">
+        <h1 className="text-white font-bold text-2xl text-center pt-10 pb-10">
+          My Decisions
+        </h1>
+        <div className="my-32 flex flex-row  justify-around overflow-x-scroll ">
+          <Card />
+          <Card />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
