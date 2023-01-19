43b604348a5eecf722f4ef76c425443802b328dc
Tony Creavin <tonycreavin@gmail.com>
2023-01-19 15:06:49 +0100

    fixed sidebar and burger button issue

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -5,8 +5,8 @@ import { useAuth } from "../context/UserContext";
 export default function SideBar() {
   const { signOut } = useAuth();
   return (
-    <div className="h-[300px] absolute right-0 z-50 w-1/4 float-right  bg-[#063944] rounded-l-[70px] flex flex-col justify-around items-end pr-5 text-lg text-yellow-400">
-      <ul>
+    <div className="h-[300px] absolute right-0 z-50 w-[200px] float-right  bg-[#063944] rounded-l-[70px] flex flex-col justify-around items-end pr-5 text-lg text-yellow-400">
+      <ul className="space-y-5 ">
         <li>
           <Link href="/">Home</Link>
         </li>
